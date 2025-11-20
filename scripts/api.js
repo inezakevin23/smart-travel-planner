c93063caacc1b4c988c6836c4c461aecb83f31e4
inezakevin <k.ineza1@alustudent.com>
2025-11-20 22:56:44 +0200

    deleted search.js and script.js file as they no longer needed

--- scripts/api.js
+++ scripts/api.js
@@ -276,7 +276,6 @@ const ApiService = {
       }
       await Promise.all(promises);
 
-      // Return the aggregated results
       return results;
     } catch (error) {
       console.error("Error fetching destination data:", error);
